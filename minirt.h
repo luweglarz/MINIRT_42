fd2a73c0c904ee8fd6fe3b75da1dad1323b69f24
RolandGamos <lucasweglarz9@gmail.com>
2020-10-13 16:17:20 +0200

    init minirt.h

---
+++ minirt.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/13 15:56:38 by lweglarz          #+#    #+#             */
+/*   Updated: 2020/10/13 16:05:11 by lweglarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+#include <math.h>
+#include <stdlib.h>
+
+#include "../MA_LIBFT42/libft.h"
+#include "mlx.h"
+#include "scene.h"
+
+#endif
+
