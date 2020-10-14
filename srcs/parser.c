bdb6e0cbb6b0a1a2d5e732cd98e529e8e9236744
RolandGamos <lucasweglarz9@gmail.com>
2020-10-14 15:52:11 +0200

    parser update

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,13 +6,13 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 12:52:23 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/14 15:16:21 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/14 15:48:29 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
-void    init_scene(t_scene *scene)
+void        init_scene(t_scene *scene)
 {
     scene->resolution.h = 0;
     scene->resolution.w = 0;
@@ -23,7 +23,12 @@ void    init_scene(t_scene *scene)
     
 }
 
-void    parse(int fd)
+int         append_data(t_scene **scene, char *line)
+{
+    //fonction d'attribution des donne recuperer avec get next line
+}
+
+t_scene     *parsing_config(int fd)
 {
     t_scene *scene;
     char    *line;
@@ -32,7 +37,10 @@ void    parse(int fd)
 
     while ((ret = get_next_line(fd, &line)) > 0)
     {
-        
-
+        if (!(ret = append_data(&scene, line)) == -1)
+            return (NULL);
+        free(line);
     }
+    return (scene);
 }
+
