7e2a9c1037b0baa545045d683462e05a8d402bf4
RolandGamos <lucasweglarz9@gmail.com>
2020-10-14 16:34:26 +0200

    added functions

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 13:42:16 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/14 15:49:58 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/14 16:26:59 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ typedef struct		s_list
 
 t_scene     *parsing_config(int fd);
 void        init_scene(t_scene *scene);
-int         append_data(t_scene **scene, char *line);
-
+t_scene     append_data(t_scene **scene, char *line);
+t_scene     get_resolution(t_scene **scene, char *line);
 #endif
