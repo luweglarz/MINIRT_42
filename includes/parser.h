ad7cdd70aeae70a812ee6fdd6dc3082a04ca1871
RolandGamos <lucasweglarz9@gmail.com>
2020-10-14 15:51:48 +0200

    added function and rename

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 13:42:16 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/14 14:02:13 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/14 15:49:58 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,9 @@ typedef struct		s_list
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+t_scene     *parsing_config(int fd);
+void        init_scene(t_scene *scene);
+int         append_data(t_scene **scene, char *line);
 
 #endif
