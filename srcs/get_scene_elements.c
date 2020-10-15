fe283c2876424cdc96f416d80aaadb24de1d3161
RolandGamos <lucasweglarz9@gmail.com>
2020-10-15 12:52:57 +0200

    added some proto

--- srcs/get_scene_elements.c
+++ srcs/get_scene_elements.c
@@ -6,14 +6,22 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 16:24:26 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/14 16:26:40 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/15 12:51:20 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
-t_scene get_resolution(t_scene **scene, char *line)
+//fonctions qui vont recuperer les donne en parsant la line aue j'envoie
+
+t_scene get_resolution(t_scene *scene, char *line)
 {
 
 
+}
+
+t_scene get_sphere(t_scene *scene, char *line)
+{
+
+    
 }
