bd7f131baffb5af71e45c35609c838a2ffb87a41
RolandGamos <lucasweglarz9@gmail.com>
2020-10-15 16:35:51 +0200

    if forest to change ?

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 12:52:23 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/15 14:39:32 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/15 16:32:23 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,32 @@ void        init_scene(t_scene **scene)
 {
     (*scene)->resolution = NULL;
     (*scene)->amli = NULL;
-    // a finir avec list chaine pour les autres config
+    (*scene)->camera = NULL;
+    (*scene)->sphere = NULL;
 }
 
 t_scene     append_data(t_scene **scene, char *line)
 {
-    (void)scene;
-    (void)line;
     //trouver un moyen de faire ca sans foret de if 
-    if (line[0] == RES)
-        return (get_resolution(scene, line));
-    if (ft_strncmp(line, SPHERE, 2) == 0)
-        return (get_sphere(scene, line));
+    // utilister ft_split ?
+    char **data;
+    data = ft_split(line, ' '); // add une fonction qui detecte les espaces
+    if (data[0] == RES)
+        return (get_resolution(scene, data));
+    if (data[0] == AMLIGHT)
+        return (get_amlight(scene, data));
+    if (data[0] == CAMERA)
+        return (get_camera(scene, data));
+    if (data[0] == LIGHT)
+        return (get_light(scene, data));
+    if (data[0] == SPHERE)
+        return (get_sphere(scene, data));
+    if (data[0] == SQUARE)
+        return (get_square(scene, data));
+    if (data[0] == CYLINDER)
+        return (get_cylinder(scene, data));
+    if (data[0] == SQUARE)
+        return (get_triangle(scene, data));
     // ainsi de suite
 }
 
