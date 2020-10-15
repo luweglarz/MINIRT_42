eb76e12b68f8edd98d4333030f47a56df8dd4f94
RolandGamos <lucasweglarz9@gmail.com>
2020-10-15 16:33:45 +0200

    file for function to get data of objects

---
+++ srcs/get_scene_objects.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_scene_objects.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/15 16:23:47 by lweglarz          #+#    #+#             */
+/*   Updated: 2020/10/15 16:25:24 by lweglarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+t_scene get_sphere(t_scene *scene, char **data)
+{
+(void)scene;
+(void)data;
+    
+}
+
+t_scene get_square(t_scene *scene, char **data)
+{
+(void)scene;
+(void)data;
+    
+}
+
+t_scene get_cylinder(t_scene *scene, char **data)
+{
+(void)scene;
+(void)data;
+    
+}
+
+t_scene get_triangle(t_scene *scene, char **data)
+{
+(void)scene;
+(void)data;
+    
+}
