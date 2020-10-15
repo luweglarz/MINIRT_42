60a82419a2e3f44a6aaa5c7b02dbadb4ad20c08f
RolandGamos <lucasweglarz9@gmail.com>
2020-10-15 12:52:29 +0200

    define test & added functions

--- includes/parser.h
+++ includes/parser.h
@@ -6,12 +6,15 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 13:42:16 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/14 16:26:59 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/15 12:23:11 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PASTER_H
+
+#define SPHERE "sp"
+#define RES 'R'
 
 #include "minirt.h"
 
@@ -24,5 +27,6 @@ typedef struct		s_list
 t_scene     *parsing_config(int fd);
 void        init_scene(t_scene *scene);
 t_scene     append_data(t_scene **scene, char *line);
-t_scene     get_resolution(t_scene **scene, char *line);
+t_scene 	get_resolution(t_scene *scene, char *line);
+t_scene 	get_sphere(t_scene *scene, char *line);
 #endif
