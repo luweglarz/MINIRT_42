a92629868ae6f2239c33767fa9d36a5bcda5d7a1
RolandGamos <lucasweglarz9@gmail.com>
2020-10-20 13:01:10 +0200

    segfault in append of datas

--- srcs/get_scene_elements.c
+++ srcs/get_scene_elements.c
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 16:24:26 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/15 16:27:34 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/20 12:50:57 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,31 @@
 
 //fonctions qui vont recuperer les donne en parsant le tableau de data que j'envoie
 
-t_scene get_resolution(t_scene *scene, char **data)
+t_scene *get_resolution(t_scene *scene, char **data)
 {
-(void)scene;
-(void)data;
-
+    scene->resolution->w = ft_atoi(data[1]);
+    scene->resolution->h = ft_atoi(data[2]);
+    return (scene);
 }
 
-t_scene get_amlight(t_scene *scene, char **data)
+t_scene *get_amlight(t_scene *scene, char **data)
 {
-(void)scene;
+
 (void)data;
-    
+    return(scene);
 }
 
-t_scene get_camera(t_scene *scene, char **data)
+t_scene *get_camera(t_scene *scene, char **data)
 {
 (void)scene;
 (void)data;
-    
+    return(scene);
 }
 
-t_scene get_light(t_scene *scene, char **data)
+t_scene *get_light(t_scene *scene, char **data)
 {
 (void)scene;
 (void)data;
-    
+    return(scene);
 }
 
--- srcs/get_scene_objects.c
+++ srcs/get_scene_objects.c
@@ -6,36 +6,36 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 16:23:47 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/15 16:25:24 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/20 12:15:05 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minirt.h"
+#include "../includes/minirt.h"
 
-t_scene get_sphere(t_scene *scene, char **data)
+t_scene *get_sphere(t_scene *scene, char **data)
 {
 (void)scene;
 (void)data;
-    
+    return(scene);
 }
 
-t_scene get_square(t_scene *scene, char **data)
+t_scene *get_square(t_scene *scene, char **data)
 {
 (void)scene;
 (void)data;
-    
+    return(scene);
 }
 
-t_scene get_cylinder(t_scene *scene, char **data)
+t_scene *get_cylinder(t_scene *scene, char **data)
 {
 (void)scene;
 (void)data;
-    
+    return(scene);
 }
 
-t_scene get_triangle(t_scene *scene, char **data)
+t_scene *get_triangle(t_scene *scene, char **data)
 {
 (void)scene;
 (void)data;
-    
+    return(scene);
 }
