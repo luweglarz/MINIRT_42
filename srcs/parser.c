31331819e3c64e901ae21f6f0a319acf99c4545f
RolandGamos <lucasweglarz9@gmail.com>
2020-10-22 13:02:54 +0200

    normed but line 16: 86 && line 18: 89

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,31 +6,44 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 12:52:23 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/20 17:42:03 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/22 12:57:09 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
 
-void       *init_scene(t_scene *scene)
+t_tab		g_tab[9] = {
+	{RES, 1, &get_resolution}, {AMLIGHT, 1, &get_amlight}, {CAMERA, 1, &get_camera},
+	{LIGHT, 1, &get_light}, {SPHERE, 2, &get_sphere}, {PLANE, 2, &get_plane},
+	{SQUARE, 2, &get_square}, {CYLINDER, 2, &get_cylinder}, {TRIANLGE, 2, &get_triangle}
+};
+
+void	init_scene(t_scene *scene)
 {
-    scene->resolution.h = 0;
-    scene->resolution.w = 0;
-    scene->amli.ratio = 0;
-    scene->amli.color.r = 0;
-    scene->amli.color.g = 0;
-    scene->amli.color.b = 0;  
-    scene->camera = NULL;
-    scene->light = NULL;
-    scene->sphere = NULL;
-    return (scene);
+	scene->resolution.h = 0;
+	scene->resolution.w = 0;
+	scene->amli.ratio = 0;
+	scene->amli.color.r = 0;
+	scene->amli.color.g = 0;
+	scene->amli.color.b = 0;
+	scene->camera = NULL;
+	scene->light = NULL;
+	scene->sphere = NULL;
+	scene->plane = NULL;
+	scene->square = NULL;
+	scene->cylinder = NULL;
+	scene->triangle = NULL;
 }
 
-void		free_data(int data_nb, char **data)
+void	free_data(char **data)
 {
-	int	i;
+	int		i;
+	int		data_nb;
 
 	i = 0;
+	data_nb = 0;
+	while (data[data_nb])
+		data_nb++;
 	while (i < data_nb)
 	{
 		free(data[i]);
@@ -39,58 +52,34 @@ void		free_data(int data_nb, char **data)
 	free(data);
 }
 
-t_scene     *append_data(t_scene *scene, char *line)
+void	append_data(t_scene *scene, char *line)
 {
-    //trouver un moyen de faire ca sans foret de if 
-    char **data;
-    int    data_nb;
+	char	**data;
+	int		i;
 
-    data_nb = 0;
-    data = ft_split(line, ' ');
-    while (data[data_nb])
-        data_nb++;
-    if (data[0][0] == RES)
-        return (get_resolution(scene, data));
-    else if (data[0][0] == AMLIGHT)
-        return (get_amlight(scene, data));
-    else if (data[0][0] == CAMERA)
-        return (get_camera(scene, data));
-    else if (data[0][0] == LIGHT)
-        return (get_light(scene, data));
-    else if (data[0] == SPHERE)
-        return (get_sphere(scene, data));
-    else if (data[0] == SQUARE)
-        return (get_square(scene, data));
-    else if (data[0] == CYLINDER)
-        return (get_cylinder(scene, data));
-    else if (data[0] == SQUARE)
-        return (get_triangle(scene, data));
-    else
-        write(2, "error", 5);
-    free_data(data_nb, data);
-    return (scene);
+	data = ft_split(line, ' ');
+	i = 0;
+	while ((i < 9) &&
+			(ft_strncmp(g_tab[i].data_type, data[0], g_tab[i].data_len) != 0))
+		i++;
+	if (i <= 9)
+		g_tab[i].func(scene, data);
+	free_data(data);
 }
 
-//gestion d'erreur a faire ??
-t_scene     *parsing_config(int fd)
+void	parsing_config(t_scene *scene, int fd)
 {
-    t_scene *scene;
-    char    *line;
-    int     ret;
+	char	*line;
+	int		ret;
 
-    if (!(scene = malloc(sizeof(*scene))))
-		write(2,"error", 5);
-	if (!(init_scene(scene)))
-		return (NULL);
-    //init_scene(&scene);
-    while ((ret = get_next_line(fd, &line)) > 0)
-    {
-        if (ret != 0)
-            scene = append_data(scene, line);
-        free(line);
-        line = NULL;
-    }
-     printf("reso: %i\n", scene->resolution.w);
-    return (scene);
+	if (!(scene = malloc(sizeof(*scene))))
+		write(2, "error", 5);
+	init_scene(scene);
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		if (ret != 0)
+			append_data(scene, line);
+		free(line);
+		line = NULL;
+	}
 }
-
