45cfbebd61f80d3bea847ebfdf06557335bbe381
RolandGamos <lucasweglarz9@gmail.com>
2020-10-22 13:07:26 +0200

    main normed

--- main.c
+++ main.c
@@ -6,24 +6,24 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 15:30:07 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/20 17:42:12 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/22 13:07:14 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minirt.h"
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    t_scene *scene;
-    int fd; //  gestion d erreur a faire
-    
-    if (argc == 2)
-    {
-        fd = open(argv[1], O_RDONLY); // pareil
-        scene = parsing_config(fd);
-        close(fd);
-    }
-    else
-        write(STDERR_FILENO, "error", 5);
+	t_scene	scene;
+	int		fd;
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		parsing_config(&scene, fd);
+		close(fd);
+	}
+	else
+		write(STDERR_FILENO, "error", 5);
 }
