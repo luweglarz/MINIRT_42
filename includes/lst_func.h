7a85eb7e270f669fd55ef295296514780685baaa
RolandGamos <lucasweglarz9@gmail.com>
2020-10-23 13:04:30 +0200

    chain list functions init

---
+++ includes/lst_func.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_func.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/23 13:01:27 by lweglarz          #+#    #+#             */
+/*   Updated: 2020/10/23 13:03:24 by lweglarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LST_FUNC_H
+# define LST_FUNC_H
+
+# include "minirt.h"
+
+typedef struct		s_list
+{
+	void	*content;
+	struct	s_list   *next;
+}					t_list;
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_camera **alst, t_list *new);
+
+#endif
---
+++ srcs/lst_func.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_func.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/23 13:00:54 by lweglarz          #+#    #+#             */
+/*   Updated: 2020/10/23 13:02:55 by lweglarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/lst_func.h"
+
+void	ft_lstadd_front(t_camera **alst, t_list *new)
+{
+	if (!*alst)
+	{
+		*alst = new;
+		return ;
+	}
+	if (!new)
+		return ;
+	new->next = *alst;
+	*alst = new;
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_elem;
+
+	if (!(new_elem = malloc(sizeof(t_list))))
+		return (NULL);
+	new_elem->content = content;
+	new_elem->next = NULL;
+	return (new_elem);
+}
