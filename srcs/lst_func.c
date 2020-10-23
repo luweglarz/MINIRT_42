e4568c054cc627056f86a4563cd74119d9ad9274
RolandGamos <lucasweglarz9@gmail.com>
2020-10-23 14:24:02 +0200

    chained list functions

--- srcs/lst_func.c
+++ srcs/lst_func.c
@@ -6,24 +6,11 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/23 13:00:54 by lweglarz          #+#    #+#             */
-/*   Updated: 2020/10/23 13:02:55 by lweglarz         ###   ########.fr       */
+/*   Updated: 2020/10/23 13:35:15 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/lst_func.h"
-
-void	ft_lstadd_front(t_camera **alst, t_list *new)
-{
-	if (!*alst)
-	{
-		*alst = new;
-		return ;
-	}
-	if (!new)
-		return ;
-	new->next = *alst;
-	*alst = new;
-}
+#include "../includes/minirt.h"
 
 t_list	*ft_lstnew(void *content)
 {
@@ -34,4 +21,17 @@ t_list	*ft_lstnew(void *content)
 	new_elem->content = content;
 	new_elem->next = NULL;
 	return (new_elem);
+}
+
+void	ft_lstadd_front(t_list **alst, t_list *new)
+{
+	if (!*alst)
+	{
+		*alst = new;
+		return ;
+	}
+	if (!new)
+		return ;
+	new->next = *alst;
+	*alst = new;
 }
