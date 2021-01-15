97c2577a2943ce7ea27db2e697a0a38da9beaf03
RolandGamos <lucasweglarz9@gmail.com>
2021-01-15 12:03:19 +0100

    parser directory created and files moved

---
+++ srcs/parser/get_scene_elements.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_scene_elements.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/14 16:24:26 by lweglarz          #+#    #+#             */
+/*   Updated: 2020/10/28 13:37:55 by lweglarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minirt.h"
+
+void	get_resolution(t_scene *scene, char **data)
+{
+	scene->reso.h = ft_atoi(data[1]);
+	scene->reso.w = ft_atoi(data[2]);
+	if (scene->reso.h <= 0 || scene->reso.w <= 0)
+		error(ERR_ELEMENT);
+}
+
+void	get_amlight(t_scene *scene, char **data)
+{
+	char	**rgb;
+	t_amli	*amlight;
+
+	if (!(amlight = malloc(sizeof(t_amli))))
+		error(ERR_MALLOC);
+	rgb = ft_split(data[2], ',');
+	amlight->color = *get_color(rgb);
+	scene->amli.ratio = ft_atof(data[1]);
+	scene->amli = *amlight;
+}
+
+void	get_camera(t_scene *scene, char **data)
+{
+	t_camera	*new_cam;
+	char		**cord;
+	char		**ori;
+
+	if (!(new_cam = malloc(sizeof(t_camera))))
+		error(ERR_MALLOC);
+	cord = ft_split(data[1], ',');
+	ori = ft_split(data[2], ',');
+	new_cam->cord = *get_cord(cord);
+	new_cam->ori = *get_orientation(ori);
+	new_cam->fov = ft_atoi(data[3]);
+	ft_lstadd_front(&scene->camera, ft_lstnew(new_cam));
+}
+
+void	get_light(t_scene *scene, char **data)
+{
+	t_light		*new_light;
+	char		**cord;
+	char		**rgb;
+
+	if (!(new_light = malloc(sizeof(t_light))))
+		error(ERR_MALLOC);
+	cord = ft_split(data[1], ',');
+	rgb = ft_split(data[3], ',');
+	new_light->cord = *get_cord(cord);
+	new_light->color = *get_color(rgb);
+	new_light->ratio = ft_atof(data[2]);
+	if (new_light->ratio < 0.0 || new_light->ratio > 1.0)
+		error(ERR_ELEMENT);
+	ft_lstadd_front(&scene->light, ft_lstnew(new_light));
+}
---
+++ srcs/parser/parsing_error.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_error.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/28 11:38:38 by lweglarz          #+#    #+#             */
+/*   Updated: 2020/10/28 13:18:19 by lweglarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minirt.h"
+
+void	error(int errorcode)
+{
+    // a voir fonction perror et strerror
+	if (errorcode == ERR_NBFILE)
+		write(STDERR_FILENO, "Too much arguments", 18);
+	if (errorcode == ERR_FEWFILE)
+		write(STDERR_FILENO, "Too few arguments", 17);
+	if (errorcode == ERR_SAVE)
+		write(STDERR_FILENO, "3rd argument must be \"-save\"", 29);
+	if (errorcode == ERR_MALLOC)
+		write(STDERR_FILENO, "Malloc failed", 13);
+	if (errorcode == ERR_ELEMENT)
+		write(STDERR_FILENO, "Wrong element value given", 25);
+	exit(errorcode);
+}
